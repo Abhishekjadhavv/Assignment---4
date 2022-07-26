b1733d2772d9f3cbf2f1b0298e360ea4006c2c4c
Abhishek jadhav <109812422+Abhishekjadhavv@users.noreply.github.com>
2022-07-26 13:48:13 +0530

    Add files via upload

---
+++ odd_num_rev.c
@@ -0,0 +1,13 @@
+// Write a program to print the first 10 odd natural numbers in reverse order.
+
+#include <stdio.h>
+
+int main()
+{
+
+    for (int i = 10; i > 0; i--)
+    {
+        printf("%d ", i * 2 - 1);
+    }
+    return 0;
+}
